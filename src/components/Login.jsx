2ab7d38f2b8a249f9afbc7acb6eadcf25a722ef0
Anudeep Reddy Bondugula <anudeepreddybondugula@gmail.com>
2023-09-11 08:29:02 +0530

    Created Register Login Basic Structure

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+
+function Login() {
+
+    return <div>
+        <h1>Login to admin dashboard</h1>
+        <br/>
+        Email - <input type={"text"} />
+        <br/>
+        <button>Login</button>
+        <br/>
+        New here? <a href="/register">Register</a>
+    </div>
+}
+
+export default Login;
