01a97db0ee1e3f51612a3345aa5dedb6b8fab237
vamshinallamothu <20eg112131@anurag.edu.in>
2023-09-14 13:25:57 +0530

    changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,37 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 
-
+//import {Container, Paper, Button } from '@mui/material';
+import {Container, Button} from '@mui/material';
 function Welcome() {
-    return <div>
-        <h1>Welcome to Digital Certificate System!</h1>
-        <a href="/register">Register</a>
-        <br/>
-        <a href="/login">Login</a>
-    </div>
+    return (
+        <>
+		<div className='main' >
+
+            <h1>Welcome to Digital Certificate System!!</h1>
+        
+        <div className='div'>
+        <Container>
+	  <div className='user'>
+		<p style={{fontSize : '40px'}}>Login for Users</p>
+                    <Button variant="contained" color="primary" onClick={() => window.location.href = '/login'}>
+                           Login
+                     </Button>
+        </div>
+        </Container>
+        <Container>
+	  <div className='org'>
+		<p style= {{ fontSize : '40px'}}>Login for Organisation</p>
+                    <Button variant="contained" color="primary" onClick={() => window.location.href = '/login'}>
+                           Login
+                     </Button>
+	  </div>
+      </Container>
+
+        </div>
+
+	  </div>
+        </>
+    )
 }
 
 export default Welcome;
