6dc0b22e609e470624d07a1fd4cac723e31ec756
Anudeep Reddy Bondugula <anudeepreddybondugula@gmail.com>
2023-09-21 03:37:37 +0530

    Added Download Certificates

--- src/components/student/Certificates.jsx
+++ src/components/student/Certificates.jsx
@@ -1,5 +1,15 @@
-import { useEffect, useState } from 'react';
-import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { useEffect, useState } from "react";
+import {
+  Container,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 const Certificates = () => {
   const [certificateData, setCertificateData] = useState([]);
@@ -8,19 +18,38 @@ const Certificates = () => {
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:3000/getCertificates", {
-            method : "GET",
-            headers : {
-                authorization : sessionStorage.getItem('jwToken')
-            }
-        })
-        setCertificateData(await response.json())
+          method: "GET",
+          headers: {
+            authorization: sessionStorage.getItem("jwToken"),
+          },
+        });
+        setCertificateData(await response.json());
       } catch (error) {
-        console.error('Error fetching certificate data:', error);
+        console.error("Error fetching certificate data:", error);
       }
     };
 
     fetchData();
   }, []);
+
+  const downloadCertificate = async (cid) => {
+    const response = await fetch("http://localhost:3000/downloadCertificate", {
+      method: "POST",
+      headers: {
+        authorization: sessionStorage.getItem("jwToken"),
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({ certificate_cid: cid }),
+    });
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "certificate.pdf";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
 
   return (
     <Container maxWidth="md">
@@ -39,7 +68,11 @@ const Certificates = () => {
           </TableHead>
           <TableBody>
             {certificateData.map((certificate, index) => (
-              <TableRow key={index}>
+              <TableRow
+                key={index}
+                onClick={() =>
+                  downloadCertificate(certificate.certificate_cid)
+                }>
                 <TableCell>{certificate.certificate_name}</TableCell>
                 <TableCell>{certificate.issuedBy}</TableCell>
                 <TableCell>{certificate.issuedDate}</TableCell>
